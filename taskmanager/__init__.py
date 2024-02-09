4316ee3e2015129988fc87a6e28083ee41cec20b
Megan Walford <mwalford22@gmail.com>
2024-02-09 17:12:26 +0000

    Addition of styles and js

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,9 +12,3 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
-from taskmanager import app, db
-
-# Use app.app_context() to set up the Flask application context
-with app.app_context():
-    # Create all database tables
-    db.create_all()
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //side nav initialisation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
